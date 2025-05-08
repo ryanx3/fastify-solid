5f2c40ded800f97d8eae1e777de50de7411126af
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-05-08 21:22:41 +0100

    criando a primeira rota para testar o endpoint users e configurado o prisma para detalhar logs no console em desenvolvimento

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
-import { PrismaClient } from '../generated/prisma'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
-export const prisma = new PrismaClient()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
