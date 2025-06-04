ecac36a3af1ee87eaa1c1232c46951594ad0ab2e
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-06-04 21:34:46 +0100

    criado novo metodo para buscar usuario por id na interface

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,30 +1,40 @@
-import { UsersRepository } from '@/repositories/users-repository'
-import type { Prisma, User } from 'generated/prisma'
+import { UsersRepository } from "@/repositories/users-repository";
+import type { Prisma, User } from "generated/prisma";
 
 export class InMemoryUsersRepository implements UsersRepository {
-  public items: User[] = []
+  public items: User[] = [];
 
   async findByEmail(email: string) {
-    const user = this.items.find((item) => item.email === email)
+    const user = this.items.find((item) => item.email === email);
 
     if (!user) {
-      return null
+      return null;
     }
 
-    return user
+    return user;
+  }
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
   }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: "user-1",
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date(),
-    }
+    };
 
-    this.items.push(user)
+    this.items.push(user);
 
-    return user
+    return user;
   }
 }
--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,6 +1,7 @@
-import type { Prisma, User } from 'generated/prisma'
+import type { Prisma, User } from "generated/prisma";
 
 export interface UsersRepository {
-  findByEmail: (email: string) => Promise<User | null>
-  create: (data: Prisma.UserCreateInput) => Promise<User>
+  findByEmail: (email: string) => Promise<User | null>;
+  findById: (id: string) => Promise<User | null>;
+  create: (data: Prisma.UserCreateInput) => Promise<User>;
 }
