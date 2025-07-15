e465687fe61e5e08bd64ecb241d205d9b23ac2a1
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-07-15 07:02:58 +0100

    Adiciona método 'countByUserId' para buscar todas as métricas de check-in de um usuário específico.

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,7 @@ import type { Prisma, CheckIn } from "generated/prisma";
 
 export interface CheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
-  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+  findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>;
+  countByUserId: (userId: string) => Promise<number>;
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>;
 }
