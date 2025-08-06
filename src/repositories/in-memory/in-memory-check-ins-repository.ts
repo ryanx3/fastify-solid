02c38a39fdcc091cf35754ad35c60432fa4494c5
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-06 14:08:34 +0100

    feat: criado validação de checkin e testes para verificar se existe um validated_at para confirmar checkin e caso não existe gere o erro

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,16 +6,12 @@ import dayjs from "dayjs";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
-    const checkIn = {
-      id: randomUUID(),
-      validated_at: data.validated_at ? new Date(data.validated_at) : null,
-      user_id: data.user_id,
-      gym_id: data.gym_id,
-      created_at: new Date(),
-    };
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
 
-    this.items.push(checkIn);
+    if (!checkIn) {
+      return null;
+    }
 
     return checkIn;
   }
@@ -24,10 +20,6 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.items
       .filter((item) => item.user_id === userId)
       .slice((page - 1) * 20, page * 20);
-  }
-
-  async countByUserId(userId: string) {
-    return this.items.filter((item) => item.user_id === userId).length;
   }
 
   async findByUserIdOnDate(userId: string, date: Date) {
@@ -47,5 +39,33 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
 
     return checkInOnSameDate;
+  }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      created_at: new Date(),
+    };
+
+    this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+
+    return checkIn;
   }
 }
