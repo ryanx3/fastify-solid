13b8649d796d941a53b7f7982693a8b078a710ef
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-12 07:10:04 +0100

    feat: criado rota e controller para profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
 
 export function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
 
   app.post("/sessions", authenticate);
+
+  /** Authenticated */
+  app.get("/me", profile);
 }
