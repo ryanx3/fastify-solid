e70b434c2e279b0e90be00f19f88d4e7e6f8c574
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-14 06:50:52 +0100

    test: criado test e2e para a rota de autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -11,12 +11,17 @@ describe("Register (e2e)", () => {
     await app.close(); // Aguardamos o app fechar
   });
 
-  it("it should be able to register", async () => {
-    const response = await request(app.server).post("/users").send({
+  it("it should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
       name: "John doe",
       email: "johndoe@example.com",
       password: "123456",
     });
-    expect(response.statusCode).toBe(201);
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ token: expect.any(String) });
   });
 });
