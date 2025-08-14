0f9d54d865a100e9a86c149b53d817735e3aef13
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-14 07:18:48 +0100

    feat: criado controller para busca de academias perto dos usuarios e rotas para cada função

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import type { FastifyInstance } from "fastify";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { search } from "./search";
+import { nearby } from "./nearby";
+import { create } from "./create";
 
 export function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT); // Com isso, todas as rotas terão que verificar o jwt, ou seja, estar logado
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -3,12 +3,12 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
-  const searchGymsBodySchema = z.object({
+  const searchGymsQuerySchema = z.object({
     query: z.string(),
     page: z.coerce.number().min(1).default(1),
   });
 
-  const { query, page } = searchGymsBodySchema.parse(request.body);
+  const { query, page } = searchGymsQuerySchema.parse(request.body);
 
   const searchGymsUseCase = makeSearchGymsUseCase();
   const { gyms } = await searchGymsUseCase.execute({ query, page });
