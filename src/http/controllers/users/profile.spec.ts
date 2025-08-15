ea1b9a62a26ccbc3f87722db4703e2ae8e1b65b9
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-15 09:29:16 +0100

    feat: automatizado a autenticação do usuário para testes e2e

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import request from "supertest";
 import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Register (e2e)", () => {
   beforeAll(async () => {
@@ -12,17 +13,7 @@ describe("Register (e2e)", () => {
   });
 
   it("it should be able to get user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "John doe",
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
