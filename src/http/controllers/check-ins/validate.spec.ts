3a9923e1ad91949e4e9d50a177a759324438b28d
ryanx3 <ryangabriel_cap@hotmail.com>
2025-08-16 18:15:14 +0100

    test: alterado função de autenticar usuário para poder passar a role do usuário durante os testes. Com isso, corrigi os testes que falharam por contas das roles de usuario

--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -14,7 +14,7 @@ describe("Validate Check-In (e2e)", () => {
   });
 
   it("it should be able to create a check-in", async () => {
-    const { token } = await createAndAuthenticateUser(app);
+    const { token } = await createAndAuthenticateUser(app, true);
 
     const user = await prisma.user.findFirstOrThrow();
     const gym = await prisma.gym.create({
